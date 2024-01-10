e901b618aefab2c386db6d251b5bbe065170d1b0
evaqum <evaqum@internet.ru>
2024-01-10 19:57:27 +0300

    initial commit

---
+++ lib/features/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HotelApp extends StatelessWidget {
+  const HotelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Placeholder(),
+      ),
+    );
+  }
+}
